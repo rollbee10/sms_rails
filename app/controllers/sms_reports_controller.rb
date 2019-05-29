867d11d3a727278a5e9a76e7507b1eab9af34440
rollbee10 <47851392+rollbee10@users.noreply.github.com>
2019-05-29 23:32:22 +0200

    Update sms_reports_controller.rb

--- app/controllers/sms_reports_controller.rb
+++ app/controllers/sms_reports_controller.rb
@@ -5,12 +5,6 @@ class SmsReportsController < BaseController
     @messages = Message.where(:user_id => current_user.id, message_send_status: 1).order('id DESC')
   end
 
-  def reports
-    @results = Message.where(:user_id => current_user.id)
-    respond_to do |format|
-      format.html
-      format.csv { send_data @results.to_csv }
-    end
-  end
+ 
 
 end
